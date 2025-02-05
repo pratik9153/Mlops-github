ae5f06eba33feb857862451962e42068b9447a8d
pratik9153 <pratikchoudhary316@gmail.com>
2025-02-05 09:38:52 +0530

    second

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Ml-Pipeline",
+    version= "0.0.0",
+    author="Pratik",
+    author_email="pratikchoudhary316@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
